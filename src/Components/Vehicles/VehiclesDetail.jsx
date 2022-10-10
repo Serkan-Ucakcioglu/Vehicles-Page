1f614c9e81574bb5350e41c25c942b7d67541833
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-10-10 09:54:34 +0300

    hoc

--- src/Components/Vehicles/VehiclesDetail.jsx
+++ src/Components/Vehicles/VehiclesDetail.jsx
@@ -53,6 +53,42 @@ function VehiclesDetail() {
       setEditShow(false);
     }
   };
+
+  const content = (
+    <div className="flex flex-col items-center cursor-pointer my-4 p-6 max-w-sm text-white hover:text-black bg-slate-800 rounded border shadow-xl hover:bg-white hover:border hover:border-black dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <span className="text-sm font-bold uppercase pb-4">
+        Brand: {vehicles?.brand}
+      </span>
+      <span className="text-sm font-bold uppercase pb-4">
+        Model: {vehicles?.model}
+      </span>
+      <span className="text-sm font-bold uppercase pb-4">
+        ModelYear: {vehicles?.modelYear}
+      </span>
+      <span className="text-sm font-bold uppercase pb-4">
+        Plate: {vehicles?.plate}
+      </span>
+      <span className="text-sm font-bold uppercase pb-4">
+        Notes: {vehicles?.notes}
+      </span>
+
+      <span className="flex justify-between items-center">
+        <img
+          src={edit}
+          alt="edit"
+          className="w-10 h-5 cursor-pointer"
+          onClick={() => setEditShow(true)}
+        />
+        <img
+          src={deletes}
+          alt="delete"
+          onClick={() => setShow(true)}
+          className="w-10 h-5 cursor-pointer"
+        />
+      </span>
+    </div>
+  );
+
   if (isFetching) return <Loader />;
 
   return (
@@ -62,38 +98,7 @@ function VehiclesDetail() {
         <h1 className="mt-4 text-black text-2xl text-center mb-3 font-extrabold">
           Vehicles Details
         </h1>
-        <div className="flex flex-col items-center cursor-pointer my-4 p-6 max-w-sm text-white hover:text-black bg-slate-800 rounded border shadow-xl hover:bg-white hover:border hover:border-black dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <span className="text-sm font-bold uppercase pb-4">
-            Brand: {vehicles?.brand}
-          </span>
-          <span className="text-sm font-bold uppercase pb-4">
-            Model: {vehicles?.model}
-          </span>
-          <span className="text-sm font-bold uppercase pb-4">
-            ModelYear: {vehicles?.modelYear}
-          </span>
-          <span className="text-sm font-bold uppercase pb-4">
-            Plate: {vehicles?.plate}
-          </span>
-          <span className="text-sm font-bold uppercase pb-4">
-            Notes: {vehicles?.notes}
-          </span>
-
-          <span className="flex justify-between items-center">
-            <img
-              src={edit}
-              alt="edit"
-              className="w-10 h-5 cursor-pointer"
-              onClick={() => setEditShow(true)}
-            />
-            <img
-              src={deletes}
-              alt="delete"
-              onClick={() => setShow(true)}
-              className="w-10 h-5 cursor-pointer"
-            />
-          </span>
-        </div>
+        {content}
       </div>
 
       {/* Go Back Location */}
