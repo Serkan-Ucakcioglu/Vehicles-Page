b24749dd2e2be7e8a0f26e30ec2c5376ac113181
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-10-10 09:48:06 +0300

    input search button bg change

--- src/Components/Model/Model.jsx
+++ src/Components/Model/Model.jsx
@@ -84,7 +84,7 @@ function Model() {
           </div>
           <button
             type="submit"
-            className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="p-2.5 ml-2 text-sm font-medium text-white bg-sky-600 rounded-lg border border-blue-700 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg
               className="w-5 h-5"
