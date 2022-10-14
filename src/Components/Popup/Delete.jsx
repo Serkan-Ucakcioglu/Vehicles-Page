9f6de8eecb67c17a25e585291b93ae24aecf3811
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-10-14 10:55:03 +0300

    rounded fix

--- src/Components/Popup/Delete.jsx
+++ src/Components/Popup/Delete.jsx
@@ -16,7 +16,7 @@ function Delete({ show, setShow, onSubmit, handleSubmit }) {
             <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <form
                 onSubmit={handleSubmit(onSubmit)}
-                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                class="relative transform overflow-hidden rounded bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
               >
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div class="sm:flex sm:items-start">
